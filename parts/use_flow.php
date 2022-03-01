2f37471ed6d4a6da333ccad46f56fd3a287bda7f
fumiya.tsuji <fumiya0903613@gmail.com>
2022-03-02 01:54:57 +0900

    余白周り整理

---
+++ parts/use_flow.php
@@ -0,0 +1,27 @@
+<div class="use-flow">
+  <div class="use-flow-contents">
+    <div class="use-flow-title">ご利用の流れ</div>
+    <div class="use-flow-box">
+      <div class="use-flow-row">
+        <div class="use-flow-no">01</div>
+        <div class="use-flow-col-1">お問い合わせ</div>
+        <div class="use-flow-col-2">まずはフォームまたはお電話からお申し込みください。</div>
+      </div>
+      <div class="use-flow-row">
+        <div class="use-flow-no">02</div>
+        <div class="use-flow-col-1">ヒアリング</div>
+        <div class="use-flow-col-2">現在の学習状況やTOEFLスコア、目標のスコアをお聞きします。</div>
+      </div>
+      <div class="use-flow-row">
+        <div class="use-flow-no">03</div>
+        <div class="use-flow-col-1">学習プランのご提供</div>
+        <div class="use-flow-col-2">目標達成のために最適な学習プランをご提案致します。</div>
+      </div>
+      <div class="use-flow-row">
+        <div class="use-flow-no">04</div>
+        <div class="use-flow-col-1">ご入会</div>
+        <div class="use-flow-col-2">お申込み完了後、レッスンがスタートします。</div>
+      </div>
+    </div>
+  </div>
+</div>

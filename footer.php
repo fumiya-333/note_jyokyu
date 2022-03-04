c637841fd595a08928e048856f24aaa4a0b0d8c9
fumiya.tsuji <fumiya0903613@gmail.com>
2022-03-04 23:31:26 +0900

    レイアウト調整

--- footer.php
+++ footer.php
@@ -1,2 +1,22 @@
 <footer>
+  <div class="footer-contents">
+    <div class="footer-inner-contents">
+      <div class="inner-contents-left">
+        <ul>
+          <li><a href="">ホーム</a></li>
+          <li><a href="">お知らせ</a></li>
+          <li><a href="">ブログ</a></li>
+          <li><a href="">コース・料金</a></li>
+        </ul>
+      </div>
+      <div class="inner-contents-right">
+        <img src="<?php bloginfo('template_directory'); ?>/img/footer_logo.png">
+        <div class="phone-number">0123-456-7890</div>
+        <div class="active-time">平日08:00〜20:00</div>
+      </div>
+    </div>
+  </div>
+  <div class="footer-bottom-contents">
+    <div class="copy-right">© 2020 Engress. </div>
+  </div>
 </footer>

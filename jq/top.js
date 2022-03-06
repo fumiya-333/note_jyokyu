9ed21e42a4d698b87702605522152494c112f19c
fumiya.tsuji <fumiya0903613@gmail.com>
2022-03-06 17:16:42 +0900

    Engress強みレイアウトのレスポンシブ対応

--- jq/top.js
+++ jq/top.js
@@ -21,13 +21,15 @@ Top.prototype.responsiveView = function(){
     $('#toefl-study-sub-title-box').html('<ul><li>・勉強の習慣が身についていない</li><li>・勉強しているはずなのに点数が伸びない</li><li>・正しい勉強方法がわからない</li></ul>');
     $('#toefl-study-box-text').html('完全オーダーメイドで、<br>１人１人の悩みに合わせた最適な指導で<br>TOEFLの苦手分野を克服します。');
 
-    $('toefl-optimization-title, toefl-optimization-title, toefl-optimization-title')
+    $('#toefl-optimization-title').html('TOEFLに最適化された無駄のないカリキュラム');
+    $('#toefl-teacher-title').html('日本人指導歴10年以上の経験豊富な講師陣');
   }else{
     $('#toefl-study-title').html('TOEFL学習でこんな悩みありませんか？');
     $('#toefl-study-sub-title-box').html('<ul><li>勉強の習慣が<br>身についていない</li><li>勉強しているはず<br>なのに点数が伸びない</li><li>正しい勉強方法が<br>わからない</li></ul>');
     $('#toefl-study-box-text').html('完全オーダーメイドで、１人１人の悩みに合わせた最適な指導で<br>TOEFLの苦手分野を克服します。');
 
-
+    $('#toefl-optimization-title').html('TOEFLに最適化された<br>無駄のないカリキュラム');
+    $('#toefl-teacher-title').html('日本人指導歴10年以上の<br>経験豊富な講師陣');
   }
 };
 

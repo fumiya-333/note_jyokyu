dbfcaae47c17adbc0db252e9452438458d695d64
fumiya.tsuji <fumiya0903613@gmail.com>
2022-03-06 18:51:16 +0900

    成功事例レイアウトのレスポンシブ対応

--- parts/toefl_success_stories.php
+++ parts/toefl_success_stories.php
@@ -4,30 +4,42 @@
     <div class="toefl-success-content">
       <div class="toefl-success-box">
         <div class="toefl-success-message"><?php the_field("toefl_ibt_1"); ?></div>
-        <div class="toefl-success-img"><img src="<?php the_field("toefl_img_1"); ?>"></div>
-        <div class="toefl-success-el-name">
-          <div class="toefl-success-el"><?php the_field("toefl_el_1"); ?></div>
-          <div class="toefl-success-name"><?php the_field("toefl_name_1"); ?></div>
+        <div class="toefl-success-individual">
+          <div class="toefl-success-img"><img src="<?php the_field("toefl_img_1"); ?>"></div>
+          <div class="toefl-success-el-wrap">
+            <div class="toefl-success-el-name">
+              <div class="toefl-success-el"><?php the_field("toefl_el_1"); ?></div>
+              <div class="toefl-success-name"><?php the_field("toefl_name_1"); ?></div>
+            </div>
+            <div class="toefl-success-result"><?php the_field("toefl_result_1"); ?></div>
+          </div>
         </div>
-        <div class="toefl-success-result"><?php the_field("toefl_result_1"); ?></div>
       </div>
       <div class="toefl-success-box">
         <div class="toefl-success-message"><?php the_field("toefl_ibt_2"); ?></div>
-        <div class="toefl-success-img"><img src="<?php the_field("toefl_img_2"); ?>"></div>
-        <div class="toefl-success-el-name">
-          <div class="toefl-success-el"><?php the_field("toefl_el_2"); ?></div>
-          <div class="toefl-success-name"><?php the_field("toefl_name_2"); ?></div>
+        <div class="toefl-success-individual">
+          <div class="toefl-success-img"><img src="<?php the_field("toefl_img_2"); ?>"></div>
+          <div class="toefl-success-el-wrap">
+            <div class="toefl-success-el-name">
+              <div class="toefl-success-el"><?php the_field("toefl_el_2"); ?></div>
+              <div class="toefl-success-name"><?php the_field("toefl_name_2"); ?></div>
+            </div>
+            <div class="toefl-success-result"><?php the_field("toefl_result_2"); ?></div>
+          </div>
         </div>
-        <div class="toefl-success-result"><?php the_field("toefl_result_2"); ?></div>
       </div>
       <div class="toefl-success-box">
         <div class="toefl-success-message"><?php the_field("toefl_ibt_3"); ?></div>
-        <div class="toefl-success-img"><img src="<?php the_field("toefl_img_3"); ?>"></div>
-        <div class="toefl-success-el-name">
-          <div class="toefl-success-el"><?php the_field("toefl_el_3"); ?></div>
-          <div class="toefl-success-name"><?php the_field("toefl_name_3"); ?></div>
+        <div class="toefl-success-individual">
+          <div class="toefl-success-img"><img src="<?php the_field("toefl_img_3"); ?>"></div>
+          <div class="toefl-success-el-wrap">
+            <div class="toefl-success-el-name">
+              <div class="toefl-success-el"><?php the_field("toefl_el_3"); ?></div>
+              <div class="toefl-success-name"><?php the_field("toefl_name_3"); ?></div>
+            </div>
+            <div class="toefl-success-result"><?php the_field("toefl_result_3"); ?></div>
+          </div>
         </div>
-        <div class="toefl-success-result"><?php the_field("toefl_result_3"); ?></div>
       </div>
     </div>
   </div>

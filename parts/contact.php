41bfa62b5e778f558c4bcc1dfc7eed30a9edd495
fumiya.tsuji <fumiya0903613@gmail.com>
2022-03-16 20:39:09 +0900

    お問い合わせ画面作成（WM WPFORM未対応）

--- parts/contact.php
+++ parts/contact.php
@@ -12,17 +12,21 @@
       <input type="text" placeholder="01-2345-6789" class="form-text">
       <label class="form-label-radio">お問い合わせの種類を選択してください（<b>資料請求の方は資料請求を選択ください</b>）</label>
       <div class="form-radios">
-        <label><input type="radio" name="contact-radio">商談のご相談</label>
-        <label><input type="radio" name="contact-radio">サービスに関するお問い合わせ</label>
-        <label><input type="radio" name="contact-radio">資料請求</label>
-        <label><input type="radio" name="contact-radio">その他</label>
+        <input type="radio" name="contact-radio" id="opportunity-contact" class="form-radio" checked="checked">
+        <label for="opportunity-contact" class="form-radio-label">商談のご相談</label>
+        <input type="radio" name="contact-radio" id="service-contact" class="form-radio">
+        <label for="service-contact" class="form-radio-label">サービスに関するお問い合わせ</label>
+        <input type="radio" name="contact-radio" id="doq-req-contact" class="form-radio">
+        <label for="doq-req-contact" class="form-radio-label">資料請求</label>
+        <input type="radio" name="contact-radio" id="others-contact" class="form-radio">
+        <label for="others-contact" class="form-radio-label">その他</label>
       </div>
       <label class="form-label-contact">お問い合わせ内容</label>
       <textarea class="textarea-contact" placeholder="入力して下さい"></textarea>
       <div class="privacy-policy-box"><a href="<?= Constants::CONTACT_URL ?>">プライバシーポリシー</a>に同意の上、送信ください。</div>
       <div class="privacy-policy-agree-box">
-        <input type="checkbox" name="privacy-policy-agree" id="privacy-policy-agree">
-        <label for="privacy-policy-agree">プライバシーポリシーに同意する</label>
+        <input type="checkbox" name="privacy-policy-agree" id="privacy-policy-agree" class="privacy-policy-agree">
+        <label for="privacy-policy-agree" class="privacy-policy-agree-label">プライバシーポリシーに同意する</label>
       </div>
       <div class="btn-send-box">
       <button class="btn-send">送信する</button>

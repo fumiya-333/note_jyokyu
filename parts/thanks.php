feb1271b6311dc64f5976b68f3ede657ad877213
fumiya.tsuji <fumiya0903613@gmail.com>
2022-03-21 19:24:25 +0900

    送信完了画面作成

--- parts/thanks.php
+++ parts/thanks.php
@@ -1,8 +1,5 @@
 <div class="thanks">
   <div class="thanks-contents">
-    <div class="thanks-explanation">お問い合わせいただきありがとうございます<br>内容を確認した後、担当者よりご連絡いたします</div>
-    <div class="home-link-area">
-      <a href="<?= HOME_URL ?>">ホームへ戻る</a>
-    </div>
+    <?= do_shortcode("[mwform_formkey key='" . Constants::FORM_ID_CONTACT . "']"); ?>
   </div>
 </div>

4f494a239ad6b65b51c84a0a5614184b61721bf6
fumiya.tsuji <fumiya0903613@gmail.com>
2022-03-28 09:51:33 +0900

    トップページレイアウト修正

--- parts/rate_plan.php
+++ parts/rate_plan.php
@@ -2,7 +2,7 @@
   <div class="rate-plan-contents">
     <div class="rate-plan-title">Engressの料金プランはこちら</div>
     <div class="btn-more-rate-plan-area">
-      <button class="btn-more-rate-plan btn-border-white" id="btn-more-rate-plan">料金を見てみる</button>
+      <button class="btn-border-white btn-more-rate-plan" id="btn-more-rate-plan">料金を見てみる</button>
     </div>
   </div>
 </section>

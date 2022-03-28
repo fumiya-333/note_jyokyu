2d3f8a7fa1f0186825e83b50f8d0a668536747ea
fumiya.tsuji <fumiya0903613@gmail.com>
2022-03-28 09:29:45 +0900

    トップページのレイアウト修正

--- parts/toefl_strong.php
+++ parts/toefl_strong.php
@@ -5,40 +5,39 @@
 <section class="toefl-strong">
   <div class="toefl-strong-contents">
     <div class="toefl-strong-title">TOEFL対策に特化したEngress3つの強み</div>
-    <div class="toefl-optimization">
-      <div class="toefl-optimization-left">
-        <div class="tip-lol tip-yellow" id="tip-lol-1">特長 １</div>
-        <div class="toefl-title" id="toefl-optimization-title">TOEFLに最適化された<br>無駄のないカリキュラム</div>
-        <div class="toefl-text" id="toefl-optimization-text">
-          TOEFLではビジネス英語には登場しない数多くの学術的内容が出題されます。そのため、ベースとなる知識も必要になります。Engressでは過去1000題を分析し、最適なカリキュラムを組んでいます。
+    <div class="toefl-strong-content">
+      <div class="toefl-strong-row">
+        <div class="toefl-strong-col-left">
+          <div class="tip-lol tip-yellow" id="tip-lol-1">特長 １</div>
+          <div class="toefl-strong-col-title" id="toefl-optimization-title">TOEFLに最適化された<br>無駄のないカリキュラム</div>
+          <div class="toefl-strong-col-text" id="toefl-optimization-text">
+            TOEFLではビジネス英語には登場しない数多くの学術的内容が出題されます。そのため、ベースとなる知識も必要になります。Engressでは過去1000題を分析し、最適なカリキュラムを組んでいます。
+          </div>
+        </div>
+        <div class="toefl-strong-col-right toefl-strong-margin-left" style="background-image: url(<?= bloginfo('template_directory'); ?>/img/feature01.jpg);">
         </div>
       </div>
-      <div class="toefl-optimization-right">
-        <img src="<?php bloginfo('template_directory'); ?>/img/feature01.jpg" alt="svg" width="<?= $toefl_strong_img_width ?>" height="<?= $toefl_strong_img_height ?>"/>
-      </div>
-    </div>
-    <div class="toefl-teacher">
-      <div class="toefl-teacher-left">
-        <img src="<?php bloginfo('template_directory'); ?>/img/feature02.jpg" alt="svg" width="<?= $toefl_strong_img_width ?>" height="<?= $toefl_strong_img_height ?>"/>
-      </div>
-      <div class="toefl-teacher-right">
-        <div class="tip-lol tip-yellow" id="tip-lol-2">特長 ２</div>
-        <div class="toefl-title" id="toefl-teacher-title">日本人指導歴10年以上の<br>経験豊富な講師陣</div>
-        <div class="toefl-text" id="toefl-teacher-text">
-          Engressの講師陣は、もともと日本人向けにTOEFLを教えていた人が大多数です。また全メンバーがTESOL(英語教授法)を取得しており、知識と経験を兼ね備えている教育のプロフェッショナルです。
+      <div class="toefl-strong-row">
+        <div class="toefl-strong-col-right" style="background-image: url(<?= bloginfo('template_directory'); ?>/img/feature02.jpg);">
+        </div>
+        <div class="toefl-strong-col-left toefl-strong-margin-left">
+          <div class="tip-lol tip-yellow" id="tip-lol-2">特長 ２</div>
+          <div class="toefl-strong-col-title" id="toefl-teacher-title">日本人指導歴10年以上の<br>経験豊富な講師陣</div>
+          <div class="toefl-strong-col-text" id="toefl-teacher-text">
+            Engressの講師陣は、もともと日本人向けにTOEFLを教えていた人が大多数です。また全メンバーがTESOL(英語教授法)を取得しており、知識と経験を兼ね備えている教育のプロフェッショナルです。
+          </div>
         </div>
       </div>
-    </div>
-    <div class="toefl-grades-up">
-      <div class="toefl-grades-left">
-        <div class="tip-lol tip-yellow" id="tip-lol-3">特長 ３</div>
-        <div class="toefl-title" id="toefl-grades-title">平均3ヶ月でTOEFL iBT20点アップ</div>
-        <div class="toefl-text" id="toefl-grades-text">
-          Engressは高校生からサラリーマンまで様々な年齢層の方々が通われていますが、完全オーダーメイドのカリキュラムで柔軟に対応しているため、平均3ヶ月でTOEFLスコアを20点アップさせています。
+      <div class="toefl-strong-row">
+        <div class="toefl-strong-col-left">
+          <div class="tip-lol tip-yellow" id="tip-lol-3">特長 ３</div>
+          <div class="toefl-strong-col-title" id="toefl-grades-title">平均3ヶ月でTOEFL iBT20点アップ</div>
+          <div class="toefl-strong-col-text" id="toefl-grades-text">
+            Engressは高校生からサラリーマンまで様々な年齢層の方々が通われていますが、完全オーダーメイドのカリキュラムで柔軟に対応しているため、平均3ヶ月でTOEFLスコアを20点アップさせています。
+          </div>
         </div>
-      </div>
-      <div class="toefl-grades-right">
-        <img src="<?php bloginfo('template_directory'); ?>/img/feature03.png" alt="svg" width="<?= $toefl_strong_img_width ?>" height="<?= $toefl_strong_img_height ?>"/>
+        <div class="toefl-strong-col-right toefl-strong-margin-left" style="background-image: url(<?= bloginfo('template_directory'); ?>/img/feature03.jpg);">
+        </div>
       </div>
     </div>
   </div>

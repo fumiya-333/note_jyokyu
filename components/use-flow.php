8192c56640cfc8b88822b558ec51959dbf725afa
fumiya.tsuji <fumiya0903613@gmail.com>
2022-05-04 11:57:08 +0900

    use-flowコンポーネント リファクタリング

--- components/use-flow.php
+++ components/use-flow.php
@@ -1,46 +1,46 @@
-<section class="use-flow">
-  <div class="use-flow-inner">
-    <div class="use-flow-title">ご利用の流れ</div>
-    <div class="use-flow-box">
-      <div class="use-flow-row">
-        <div class="use-flow-no">01</div>
-        <div class="use-flow-col-1">
+<section class="p-use-flow">
+  <div class="p-use-flow__inner">
+    <div class="p-use-flow__inner__title">ご利用の流れ</div>
+    <div class="p-use-flow__inner__list">
+      <div class="p-use-flow__inner__list__row">
+        <div class="p-use-flow__inner__list__row__no">01</div>
+        <div class="p-use-flow__inner__list__row__contact">
           お問い合わせ
-          <div class="use-flow-col-1-text">
+          <div class="p-use-flow__inner__list__row__contact__text-sp">
             まずはフォームまたはお電話からお申し込みください。
           </div>
         </div>
-        <div class="use-flow-col-2">まずはフォームまたはお電話からお申し込みください。</div>
+        <div class="p-use-flow__inner__list__row__text">まずはフォームまたはお電話からお申し込みください。</div>
       </div>
-      <div class="use-flow-row">
-        <div class="use-flow-no">02</div>
-        <div class="use-flow-col-1">
+      <div class="p-use-flow__inner__list__row">
+        <div class="p-use-flow__inner__list__row__no">02</div>
+        <div class="p-use-flow__inner__list__row__contact">
           ヒアリング
-          <div class="use-flow-col-1-text">
+          <div class="p-use-flow__inner__list__row__contact__text-sp">
             現在の学習状況やTOEFLスコア、目標のスコアをお聞きします。
           </div>
         </div>
-        <div class="use-flow-col-2">現在の学習状況やTOEFLスコア、目標のスコアをお聞きします。</div>
+        <div class="p-use-flow__inner__list__row__text">現在の学習状況やTOEFLスコア、目標のスコアをお聞きします。</div>
       </div>
-      <div class="use-flow-row">
-        <div class="use-flow-no">03</div>
-        <div class="use-flow-col-1">
+      <div class="p-use-flow__inner__list__row">
+        <div class="p-use-flow__inner__list__row__no">03</div>
+        <div class="p-use-flow__inner__list__row__contact">
           学習プランのご提供
-          <div class="use-flow-col-1-text">
+          <div class="p-use-flow__inner__list__row__contact__text-sp">
             目標達成のために最適な学習プランをご提案致します。
           </div>
         </div>
-        <div class="use-flow-col-2">目標達成のために最適な学習プランをご提案致します。</div>
+        <div class="p-use-flow__inner__list__row__text">目標達成のために最適な学習プランをご提案致します。</div>
       </div>
-      <div class="use-flow-row">
-        <div class="use-flow-no">04</div>
-        <div class="use-flow-col-1">
+      <div class="p-use-flow__inner__list__row">
+        <div class="p-use-flow__inner__list__row__no">04</div>
+        <div class="p-use-flow__inner__list__row__contact">
           ご入会
-          <div class="use-flow-col-1-text">
+          <div class="p-use-flow__inner__list__row__contact__text-sp">
             お申込み完了後、レッスンがスタートします。
           </div>
         </div>
-        <div class="use-flow-col-2">お申込み完了後、レッスンがスタートします。</div>
+        <div class="p-use-flow__inner__list__row__text">お申込み完了後、レッスンがスタートします。</div>
       </div>
     </div>
   </div>

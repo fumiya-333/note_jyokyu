a7477bf02b7fa1ec903dc3351079dc441d1c0eee
fumiya.tsuji <fumiya0903613@gmail.com>
2022-05-05 05:00:26 +0900

    コース・料金画面のリファクタリング

--- components/price-table.php
+++ components/price-table.php
@@ -1,96 +1,90 @@
-<section class="price">
-  <div class="price-inner">
-    <div class="price-box">
-      <div class="price-system-box">
-        <div class="price-system-title">料金体系</div>
-        <div class="price-system-items-box">
-          <div class="price-system-items">
-            <div class="price-system-item1">
-              入会金 39,800円
+<section class="p-price-table">
+  <div class="p-price-table__inner">
+    <div class="p-price-table__inner__wrapper-top">
+      <div class="p-price-table__inner__wrapper-top__title">料金体系</div>
+      <div class="p-price-table__inner__wrapper-top__contents">
+        <div class="p-price-table__inner__wrapper-top__contents__price">
+          入会金 39,800円
+        </div>
+        <div class="p-price-table__inner__wrapper-top__contents__icon"></div>
+        <div class="p-price-table__inner__wrapper-top__contents__price">月額費用</div>
+      </div>
+      <div class="p-price-table__inner__wrapper-top__text">Engressのカリキュラムは完全オーダーメイドのため、カリキュラム内のサポート内容によって料金が変動します。おすすめのスタンダードプランでは、進学先に合わせたサポートまで包括的に行います。</div>
+    </div>
+    <div class="p-price-table__inner__wrapper-bottom">
+      <div class="p-price-table__inner__wrapper-bottom__title">料金表</div>
+      <div class="p-price-table__inner__wrapper-bottom__contents js-scrollable">
+        <div class="p-price-table__inner__wrapper-bottom__contents__content">
+          <div class="p-price-table__inner__wrapper-bottom__contents__content__head">
+            基礎プラン
+          </div>
+          <div class="p-price-table__inner__wrapper-bottom__contents__content__body">
+            <div class="p-price-table__inner__wrapper-bottom__contents__content__body__price">
+              <?php the_field("price_1", Constants::PAGE_ID_PRICE_TABLE); ?>
             </div>
-            <div class="price-system-item2"></div>
-            <div class="price-system-item1">月額費用</div>
+            <div class="p-price-table__inner__wrapper-bottom__contents__content__body__monthly">
+              *月額（税抜価格）
+            </div>
+            <div class="p-price-table__inner__wrapper-bottom__contents__content__body__texts">
+              <div class="p-price-table__inner__wrapper-bottom__contents__content__body__texts__text">カリキュラム作成</div>
+              <div class="p-price-table__inner__wrapper-bottom__contents__content__body__texts__text">TOEFL学習サポート</div>
+              <div class="p-price-table__inner__wrapper-bottom__contents__content__body__texts__text">週一回のビデオMTG</div>
+            </div>
           </div>
         </div>
-        <div class="price-system-text">Engressのカリキュラムは完全オーダーメイドのため、カリキュラム内のサポート内容によって料金が変動します。おすすめのスタンダードプランでは、進学先に合わせたサポートまで包括的に行います。</div>
-      </div>
-      <div class="price-table-box">
-        <div class="price-table-title">料金表</div>
-        <div class="price-table-items-box">
-          <div class="price-table-items js-scrollable">
-            <div class="price-table-item">
-              <div class="price-table-head">
-                基礎プラン
-              </div>
-              <div class="price-table-body">
-                <div class="price-table-price">
-                  <?php the_field("price_1", Constants::PAGE_ID_PRICE_TABLE); ?>
-                </div>
-                <div class="price-table-caption">
-                  *月額（税抜価格）
-                </div>
-                <div class="price-table-text-box">
-                  <div class="price-table-text">カリキュラム作成</div>
-                  <div class="price-table-text">TOEFL学習サポート</div>
-                  <div class="price-table-text">週一回のビデオMTG</div>
-                </div>
-              </div>
+        <div class="p-price-table__inner__wrapper-bottom__contents__content">
+          <div class="p-price-table__inner__wrapper-bottom__contents__content__head">
+            演習プラン
+          </div>
+          <div class="p-price-table__inner__wrapper-bottom__contents__content__body">
+            <div class="p-price-table__inner__wrapper-bottom__contents__content__body__price">
+              <?php the_field("price_2", Constants::PAGE_ID_PRICE_TABLE); ?>
             </div>
-            <div class="price-table-item">
-              <div class="price-table-head">
-                演習プラン
-              </div>
-              <div class="price-table-body">
-                <div class="price-table-price">
-                  <?php the_field("price_2", Constants::PAGE_ID_PRICE_TABLE); ?>
-                </div>
-                <div class="price-table-caption">
-                  *月額（税抜価格）
-                </div>
-                <div class="price-table-text-box">
-                  <div class="price-table-text">カリキュラム作成</div>
-                  <div class="price-table-text">TOEFL学習サポート</div>
-                  <div class="price-table-text">週一回のビデオMTG</div>
-                  <div class="price-table-text">月二回の模試（解説<br>付き）</div>
-                </div>
-              </div>
+            <div class="p-price-table__inner__wrapper-bottom__contents__content__body__monthly">
+              *月額（税抜価格）
             </div>
-            <div class="price-table-item">
-              <div class="price-table-head recommendation">
-                おすすめ<br><br>
-                志望校対策プラン
-              </div>
-              <div class="price-table-body">
-                <div class="price-table-price price-table-recommendation">
-                  <?php the_field("price_3", Constants::PAGE_ID_PRICE_TABLE); ?>
-                </div>
-                <div class="price-table-caption">
-                  *月額（税抜価格）
-                </div>
-                <div class="price-table-text-box">
-                  <div class="price-table-text">カリキュラム作成</div>
-                  <div class="price-table-text">TOEFL学習サポート</div>
-                  <div class="price-table-text">週一回のビデオMTG</div>
-                  <div class="price-table-text">月二回の模試（解説<br>付き）</div>
-                  <div class="price-table-text">週一の英語面接対策</div>
-                </div>
-              </div>
+            <div class="p-price-table__inner__wrapper-bottom__contents__content__body__texts">
+              <div class="p-price-table__inner__wrapper-bottom__contents__content__body__texts__text">カリキュラム作成</div>
+              <div class="p-price-table__inner__wrapper-bottom__contents__content__body__texts__text">TOEFL学習サポート</div>
+              <div class="p-price-table__inner__wrapper-bottom__contents__content__body__texts__text">週一回のビデオMTG</div>
+              <div class="p-price-table__inner__wrapper-bottom__contents__content__body__texts__text">月二回の模試（解説<br>付き）</div>
             </div>
-            <div class="price-table-item">
-              <div class="price-table-head">
-                フレックスプラン
-              </div>
-              <div class="price-table-body">
-                <div class="price-table-price">
-                  <?php the_field("price_4", Constants::PAGE_ID_PRICE_TABLE); ?>
-                </div>
-                <div class="price-table-caption">
-                  *月額（税抜価格）
-                </div>
-                <div class="price-table-text-box">
-                  <div class="price-table-text-off">＊別途ご相談ください</div>
-                </div>
-              </div>
+          </div>
+        </div>
+        <div class="p-price-table__inner__wrapper-bottom__contents__content">
+          <div class="p-price-table__inner__wrapper-bottom__contents__content__head p-price-table__inner__wrapper-bottom__contents__content__recommendation">
+            おすすめ<br><br>
+            志望校対策プラン
+          </div>
+          <div class="p-price-table__inner__wrapper-bottom__contents__content__body">
+            <div class="p-price-table__inner__wrapper-bottom__contents__content__body__price p-price-table__inner__wrapper-bottom__contents__content__body__recommendation">
+              <?php the_field("price_3", Constants::PAGE_ID_PRICE_TABLE); ?>
+            </div>
+            <div class="p-price-table__inner__wrapper-bottom__contents__content__body__monthly">
+              *月額（税抜価格）
+            </div>
+            <div class="p-price-table__inner__wrapper-bottom__contents__content__body__texts">
+              <div class="p-price-table__inner__wrapper-bottom__contents__content__body__texts__text">カリキュラム作成</div>
+              <div class="p-price-table__inner__wrapper-bottom__contents__content__body__texts__text">TOEFL学習サポート</div>
+              <div class="p-price-table__inner__wrapper-bottom__contents__content__body__texts__text">週一回のビデオMTG</div>
+              <div class="p-price-table__inner__wrapper-bottom__contents__content__body__texts__text">月二回の模試（解説<br>付き）</div>
+              <div class="p-price-table__inner__wrapper-bottom__contents__content__body__texts__text">週一の英語面接対策</div>
+            </div>
+          </div>
+        </div>
+        <div class="p-price-table__inner__wrapper-bottom__contents__content">
+          <div class="p-price-table__inner__wrapper-bottom__contents__content__head">
+            フレックスプラン
+          </div>
+          <div class="p-price-table__inner__wrapper-bottom__contents__content__body">
+            <div class="p-price-table__inner__wrapper-bottom__contents__content__body__price">
+              <?php the_field("price_4", Constants::PAGE_ID_PRICE_TABLE); ?>
+            </div>
+            <div class="p-price-table__inner__wrapper-bottom__contents__content__body__monthly">
+              *月額（税抜価格）
+            </div>
+            <div class="p-price-table__inner__wrapper-bottom__contents__content__body__texts">
+              <div class="p-price-table__inner__wrapper-bottom__contents__content__body__texts__text-off">＊別途ご相談ください</div>
             </div>
           </div>
         </div>

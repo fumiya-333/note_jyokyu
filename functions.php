28758eb6891eb36ff9e3c2e7a60d795c7e7f3fc0
fumiya.tsuji <fumiya0903613@gmail.com>
2022-05-06 15:56:18 +0900

    ファイル読み込み処理リファクタリング

--- functions.php
+++ functions.php
@@ -1,18 +1,35 @@
 <?php
 define("WP_SCSS_ALWAYS_RECOMPILE", true);
+read_files();
 
-require_once get_theme_file_path("commons/constants.php");
-require_once get_theme_file_path("commons/common-util.php");
-require_once get_theme_file_path("commons/bl-helper.php");
+function read_files()
+{
+    $file_names = glob(get_template_directory() . "/commons/*");
+    foreach ($file_names as $file_name) {
+        require_once $file_name;
+    }
+}
 
 function theme_setup()
 {
     add_theme_support("post-thumbnails");
+
+    variable_setup();
 }
 
-function post_has_archive($args, $post_type)
+function variable_setup()
 {
-    return CommonUtil::postHasArchive($args, $post_type);
+    $GLOBALS["bl_helper"] = BlHelper::getInstance();
+
+    /**
+     * ヒアドキュメント内関数展開用変数
+     *
+     * @param $expression
+     * @return $expression
+     */
+    $GLOBALS["call_hear_doc_deploy_func"] = function ($expression) {
+        return $expression;
+    };
 }
 
 function my_static_breadcrumb_adder($breadcrumb_trail)
@@ -20,28 +37,21 @@ function my_static_breadcrumb_adder($breadcrumb_trail)
     CommonUtil::updateBreadcrumb($breadcrumb_trail);
 }
 
+function post_has_archive($args, $post_type)
+{
+    return CommonUtil::postHasArchive($args, $post_type);
+}
+
 function error_message($error, $key, $rule)
 {
     return CommonUtil::errorMessage($error, $key, $rule);
 }
 
-$bl_helper = BlHelper::getInstance();
-
-/**
- * ヒアドキュメント内関数展開用変数
- *
- * @param $expression
- * @return $expression
- */
-$call_hear_doc_deploy_func = function ($expression) {
-    return $expression;
-};
+add_action("after_setup_theme", "theme_setup");
+add_action("bcn_after_fill", "my_static_breadcrumb_adder");
 
 add_filter("show_admin_bar", "__return_false");
 add_filter("register_post_type_args", "post_has_archive", 10, 2);
 add_filter("mwform_error_message_mw-wp-form-" . Constants::FORM_ID_CONTACT, "error_message", 10, 3);
 
 remove_filter("the_content", "wpautop");
-
-add_action("after_setup_theme", "theme_setup");
-add_action("bcn_after_fill", "my_static_breadcrumb_adder");
